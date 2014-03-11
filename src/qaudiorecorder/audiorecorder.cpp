e4cd320b93de9e79e5469f5ba83bf3b81748e890
nMurakami <noboru.murakami@gmail.com>
2014-03-12 06:38:21 +0900

    removed unnecessary codes.

--- src/qaudiorecorder/audiorecorder.cpp
+++ src/qaudiorecorder/audiorecorder.cpp
@@ -10,13 +10,6 @@ AudioRecorder::AudioRecorder(QQuickItem *parent):
   ,m_codecIndex(0)
 {
     m_supportedCodecs.clear();
-    // By default, QQuickItem does not draw anything. If you subclass
-    // QQuickItem to create a visual item, you will need to uncomment the
-    // following line and re-implement updatePaintNode()
-
-    // setFlag(ItemHasContents, true);
-
-    qDebug() << "[AudioRecorder]created!!";
     this->initializeQAudioRecorder();
 }
 
@@ -93,21 +86,8 @@ void AudioRecorder::setCodec(int value){
 void AudioRecorder::initializeQAudioRecorder()
 {
     QStringList codecs;
+    QAudioEncoderSettings audioSettings;
 
-    QAudioDeviceInfo outDeviceInfo(QAudioDeviceInfo::defaultOutputDevice());
-    codecs = outDeviceInfo.supportedCodecs();
-    for (int i = 0; i < codecs.size(); ++i) {
-        qDebug() << "out device supported codecs" << i << codecs.at(i).toLocal8Bit().constData() << endl;
-    }
-    QAudioDeviceInfo inDeviceInfo(QAudioDeviceInfo::defaultInputDevice());
-
-    codecs = inDeviceInfo.supportedCodecs();
-    for (int i = 0; i < codecs.size(); ++i) {
-         qDebug() << "in device supported codecs" << i << codecs.at(i).toLocal8Bit().constData() << endl;
-    }
-
-
-    qDebug() << "new QAudioRecorder";
     m_audioRecorder = new QAudioRecorder(this);
 
     codecs = m_audioRecorder->supportedAudioCodecs();
@@ -116,38 +96,9 @@ void AudioRecorder::initializeQAudioRecorder()
         qDebug() << "supported codecs" << i << codecs.at(i).toLocal8Bit().constData() << endl;
     }
 
-    QAudioEncoderSettings audioSettings;
-//    audioSettings.setCodec("audio/pcm");
     qDebug() << "setCodec to " << codecs.at(0).toLocal8Bit().constData();
     audioSettings.setCodec(codecs.at(0).toLocal8Bit().constData());
     audioSettings.setQuality(QMultimedia::HighQuality);
-
     m_audioRecorder->setEncodingSettings(audioSettings);
-
-
-    qDebug() << "current path is " << QDir::currentPath();
-
-    QStringList inputs = m_audioRecorder->audioInputs();
-    QString selectedInput = m_audioRecorder->defaultAudioInput();
-
-
-//    foreach (QString input, inputs) {
-//        QString description = m_audioRecorder->audioInputDescription(input);
-//        // show descriptions to user and allow selection
-//        selectedInput = input;
-//    }
-
-    m_audioRecorder->setAudioInput(selectedInput);
-
-//    qDebug() << "isMetaDataWritable is" << m_audioRecorder->isMetaDataWritable();
-//    m_audioRecorder->setMetaData(QMediaMetaData::Title, QVariant(QLatin1String("Test Title")));
-//    m_audioRecorder->setMetaData(QMediaMetaData::AlbumTitle, QVariant(QLatin1String("Album Title")));
-//    m_audioRecorder->setMetaData(QMediaMetaData::AlbumArtist, QVariant(QLatin1String("Album Artist")));
-//    codecs = m_audioRecorder->supportedAudioCodecs();
-//    for (int i = 0; i < codecs.size(); ++i)
-//         qDebug() << "supported codecs" << i << codecs.at(i).toLocal8Bit().constData() << endl;
-//    codecs = m_audioRecorder->supportedAudioSampleRates();
-//    for (int i = 0; i < codecs.size(); ++i)
-//         qDebug() << "supported sample rates" << i << codecs.at(i).toLocal8Bit().constData() << endl;
 }
 
